e8586456b79d6293e8ed19ee04f283667157ce98
charue808 <charlesgum@gmail.com>
2025-09-25 14:32:42 -1000

    update to content collection

---
+++ src/content.config.ts
@@ -0,0 +1,24 @@
+// import the glob loader
+import { glob } from "astro/loaders";
+
+// import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
+	schema: z.object({
+		title: z.string(),
+		pubDate: z.date(),
+		description: z.string(),
+		author: z.string(),
+		image: z.object({
+			url: z.string(),
+			alt: z.string()
+		}),
+		tags: z.array(z.string())
+	})
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = { blog };

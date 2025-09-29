843e01433520536dc7049d0145b3c3b864513c68
charue808 <charlesgum@gmail.com>
2025-09-28 18:48:02 -1000

    add in robots file and sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://charlesgum.com",
-  integrations: [preact()]
+  integrations: [preact(), sitemap()]
 });
